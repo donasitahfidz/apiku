2085016e78176be20da9934db2807710a655603d
blograsyid <indowebtechnology@gmail.com>
2025-06-03 00:51:11 +0700

    first commit

---
+++ api/get-sheet.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+  const url =
+    "https://script.google.com/macros/s/AKfycbwFK5_EolFfB34107pJHG44KuhFQRyp_HP21aIiXTHkYIZC8yZiXG2Oz49KyvdPOqdr/exec";
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Gagal mengambil data" });
+  }
+}
