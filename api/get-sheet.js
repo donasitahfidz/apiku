6d43571b856647273efaf76b8447d71243887300
blograsyid <indowebtechnology@gmail.com>
2025-06-03 01:04:25 +0700

    perubahan kelima

--- api/get-sheet.js
+++ api/get-sheet.js
@@ -1,13 +1,15 @@
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
-  const url =
-    "https://script.google.com/macros/s/AKfycbyaH9ZIrIMEpyCUzsuTdvrpZP1pw-x4IlOu_pNwR38P9HbCk4TwWUFfBrSJAc7T4xsz/exec";
+  const url = "https://script.google.com/macros/s/AKfycbwFK5_EolFfB34107pJHG44KuhFQRyp_HP21aIiXTHkYIZC8yZiXG2Oz49KyvdPOqdr/exec";
+
   try {
     const response = await fetch(url);
     const data = await response.json();
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
   } catch (err) {
-    console.error("Fetch failed:", err); // ✅ Debug error di logs Vercel
+    console.error("Fetch failed:", err);
     res.status(500).json({ error: "Gagal mengambil data" });
   }
 }
